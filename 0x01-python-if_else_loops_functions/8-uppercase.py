f0c98cd0658847e82c9588714ac32be01e0c43aa
Olshea <olsheaolufsen@gmail.com>
2024-05-11 20:52:48 -0700

    bug fix

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
-def uppercase(str):
-    for char in str:
+def uppercase(s):
+    for char in s:
         ascii_val = ord(char)
-        if 97 <= ord(char) <= 122:
+        if 97 <= ascii_val <= 122:
             ascii_val -= 32
         print(chr(ascii_val), end="")
     print()
