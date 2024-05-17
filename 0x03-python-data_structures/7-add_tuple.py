ebe2a35032cbc0bbea8b5ff15a2011695e76e0c0
Olshea <olsheaolufsen@gmail.com>
2024-05-17 15:48:43 -0700

    bug fix

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,6 +4,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
     tuple_a = tuple_a + (0, 0)
     tuple_b = tuple_b + (0, 0)
 
-    sum_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    #sum_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return(tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
 
-    return sum_tuple
+    #return sum_tuple
