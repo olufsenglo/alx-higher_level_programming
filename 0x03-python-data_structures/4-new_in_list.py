d7720353fe0b833f552d3cb89e9f4e625cff8a2c
Olshea <olsheaolufsen@gmail.com>
2024-05-17 11:04:11 -0700

    fix linting

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
-    if my_list:    
+    if my_list:
         if idx < 0 or idx >= len(my_list):
-            #new_list = [my_list[i] for i in range(len(my_list))]
             new_list = [i for i in my_list]
             return new_list
 
