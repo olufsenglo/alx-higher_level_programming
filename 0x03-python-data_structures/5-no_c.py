c0aa508492aec2ded1fc6f5fde06f9eeaaff4416
Olshea <olsheaolufsen@gmail.com>
2024-05-17 11:41:44 -0700

    bug fix

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,6 +3,6 @@
 def no_c(my_string):
     new_string = ""
     for char in my_string:
-        if char.lower() != 'c':
+        if char != 'c' and char != 'C':
             new_string += char
     return new_string
