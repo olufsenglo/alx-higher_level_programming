16a8d66f477dff7fa606c153aab91ff414093e38
Olshea <olsheaolufsen@gmail.com>
2024-05-17 14:34:58 -0700

    bug fix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,6 @@
 def print_matrix_integer(matrix=[[]]):
     for outer in matrix:
         for j in range(len(outer)):
-            print('{:d}'.format(outer[j]), end=" " if j != len(outer) - 1 else "")
+            end = " " if j != len(outer) - 1 else ""
+            print('{:d}'.format(outer[j]), end=end)
         print()
