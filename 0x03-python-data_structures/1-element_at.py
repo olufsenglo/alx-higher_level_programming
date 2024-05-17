27b6a306512b54053a9c05bc93e7de252b0a8614
Olshea <olsheaolufsen@gmail.com>
2024-05-17 06:25:00 -0700

    bug fix

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -9,7 +9,3 @@ def element_at(my_list, idx):
         Element at index idx or none if negative
     """
     return None if idx < 0 or idx >= len(my_list) else my_list[idx]
-
-my_list = [1,2,3,10]
-
-print(element_at(my_list, 3))
