2905bb4d75ad9f58db6fa56dcc565b5e4533cc91
Olshea <olsheaolufsen@gmail.com>
2024-05-18 11:47:12 -0700

    bug fix

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,6 +2,3 @@
 
 def search_replace(my_list, search, replace):
     return([replace if search == i else i for i in my_list])
-
-my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
-print(search_replace(my_list, 5, 111))
