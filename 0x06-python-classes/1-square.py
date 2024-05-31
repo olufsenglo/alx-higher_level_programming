dd6042d48696deccbb122444dda0551e3533db7f
Olshea <olsheaolufsen@gmail.com>
2024-05-31 03:01:03 -0700

    1. Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Class square with size"""
+
+
+class Square:
+    """
+    Class to create square
+    Args:
+        size
+    """
+    def __init__(self, size):
+        """Constructor of Square"""
+        self.__size = size
